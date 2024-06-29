543b96014c153690cfeeaf12d98ec860c6e6b756
adminturneddevops <mlevan1992@gmail.com>
2024-06-29 06:35:34 -0400

    main

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"capipe/cmd"
+	_ "capipe/cmd/appstack"
 	_ "capipe/cmd/gitops"
 	_ "capipe/cmd/iac"
 	_ "capipe/cmd/monitoring"
+	_ "capipe/cmd/netstack"
 	_ "capipe/cmd/networking"
 	_ "capipe/cmd/security"
+	_ "capipe/cmd/virtstack"
 )
 
 func main() {
